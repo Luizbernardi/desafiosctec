6dd1bda58bd009917d0efa46415f98e074fb9bbc
Luizbernardi <luizbernardi@gmail.com>
2026-03-14 18:05:11 -0300

    criando interface para a service

---
+++ src/main/java/com/br/desafiosctec/service/EmpreendimentoService.java
@@ -0,0 +1,20 @@
+package com.br.desafiosctec.service;
+
+import com.br.desafiosctec.dto.EmpreendimentoRequestDTO;
+import com.br.desafiosctec.dto.EmpreendimentoResponseDTO;
+
+import java.util.List;
+
+public interface EmpreendimentoService {
+
+    EmpreendimentoResponseDTO criar(EmpreendimentoRequestDTO dto);
+
+    List<EmpreendimentoResponseDTO> listarTodos();
+
+    EmpreendimentoResponseDTO buscarPorId(Long id);
+
+    EmpreendimentoResponseDTO atualizar(Long id, EmpreendimentoRequestDTO dto);
+
+    void deletar(Long id);
+
+}
