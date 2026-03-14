12d402fe553e3d75434cb8c90ec0c998ff40b378
Luizbernardi <luizbernardi@gmail.com>
2026-03-14 17:27:23 -0300

    Initial Commit

---
+++ src/main/java/com/br/desafiosctec/DesafiosctecApplication.java
@@ -0,0 +1,13 @@
+package com.br.desafiosctec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiosctecApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafiosctecApplication.class, args);
+    }
+
+}
